af4afd27b3d7484f2d10cb3056c98493e7413f06
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-23 11:55:02 +0200

    Add project files.

---
+++ Thema3_Klassen_en_Objecten_Opdracht5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Thema3_Klassen_en_Objecten_Opdracht5
+{
+
+
+    class Program
+    {
+        /* OPDRACHT 5 : PROPERTIES
+
+        Schrijf de code voor een klasse Gewicht. Deze klasse biedt de mogelijkheid om,
+        voor een gegeven massa en valversnelling, het gewicht van die massa te meten.
+        
+        Deze klasse heeft de volgende attributen:
+
+        - Een naam (vb. De naam van een planeet of satteliet)
+        - De valversnelling op die planeet of satteliet in m/s2
+        - De massa waarvan je het gewicht wil berekenen
+        - Het gewicht W=m.g
+
+        De attributen moeten via properties kunnen worden gelezen en beschreven.
+        Via de properties kunnen er ook grenzen aan de attributen worden gesteld.
+
+        De klasse heeft één of meerdere constructors 
+
+        De klasse heeft een ToString-method waarmee een object zichzelf kenbaar kan maken.
+
+        Pas de klasse toe in een programma waarin je meerdere objecten aanmaakt met verschillende
+        attributen. Zoek hiervoor de valversnellingen voor een aantal planeten en sattelieten op.
+        
+        In het hoofdprogramma ga je in een lus een tabel opbouwen waarin je de gewichten 
+        op verschillende planeten toont voor oplopende massa's.  
+
+        massa   gewicht op Aarde    gewicht op Maan     gewicht op Mars     gewicht op Jupiter
+        0                ...                 ...                 ...                 ...
+        10               ...                 ...                 ...                 ...
+        20               ...                 ...                 ...                 ...
+        30               ...                 ...                 ...                 ...
+        40               ...                 ...                 ...                 ...
+        50               ...                 ...                 ...                 ...
+        */
+        static void Main()
+        {
+            Console.WriteLine();
+        }
+    }
+}
